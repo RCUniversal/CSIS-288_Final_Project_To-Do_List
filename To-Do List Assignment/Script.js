f0e81fb59c7f9ee07072032ff7123c3291e501a3
RCUniversal <59943067+RCUniversal@users.noreply.github.com>
2020-04-25 00:05:22 -0500

    Add files via upload

---
+++ To-Do List Assignment/Script.js
@@ -0,0 +1,16 @@
+/**
+* 
+* CSIS 228—Advanced Web Development
+* Final Project
+* final-project-team-4
+*     
+* Authors: 
+* Strawn,Travis
+* Cottrell,Ryan B
+* Rutland,Shelby
+*
+* Date: 04/11/2020  
+*
+* Filename: Script.js
+*
+*/
